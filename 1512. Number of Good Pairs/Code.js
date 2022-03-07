2bd621f86458b7e6d8ac849a066f166146249dde
Shoon Zhen Yong <77121875+zhenyong25@users.noreply.github.com>
2022-03-07 12:05:59 +0800

    Create Code.js

---
+++ 1512. Number of Good Pairs/Code.js
@@ -0,0 +1,12 @@
+var numIdenticalPairs = function(nums) {
+    var good = 0; 
+    for(var i=0; i<nums.length; i++){
+        for (var j=i+1; j<nums.length; j++){
+            if (nums[i]==nums[j]){
+                good += 1; 
+            }
+        }
+    }
+        
+return good; 
+};
